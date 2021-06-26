3d92c17f2e73ddc8e21a4289e115a82da656713a
BR-Sitabule <66717838+BR-Sitabule@users.noreply.github.com>
2021-06-26 16:00:28 +0200

    Update min_leap.sh

--- Topology/min_leap.sh
+++ Topology/min_leap.sh
@@ -4,25 +4,26 @@ echo "
 s/CYS   412/CYP   412/
 " >CYP.sed 
 
-sed -i -f script.sed P10635.B99990010.pdb
 #assigning protonation states of side chains for HIS, LYS, ASP, GLU based on their pka acquired from Propka
-sed -i -f HEM_RTZ.sed P10635.B99990010.pdb
+sed -i -f script.sed P10635.B99990010.pdb
 #renaming BLK (heme and thioridazine) residues as "HEM" and "RTZ" respectively
-sed -i -f CYP.sed P10635.B99990010.pdb
+sed -i -f HEM_RTZ.sed P10635.B99990010.pdb
 #labelling proximal cysteine as CYP
-grep " RTZ " P10635.B99990010.pdb | grep " 467" > RTZ_file.pdb
+sed -i -f CYP.sed P10635.B99990010.pdb
 #grep RTZ from model, grep one drug molecule (residue 467) and redirect it to RTZ_file.pdb
-antechamber -i RTZ_file.pdb -fi pdb  -o RTZ.pdb -fo pdb -rn RTZ
+grep " RTZ " P10635.B99990010.pdb | grep " 467" > RTZ_file.pdb
 #convert ligand file to amber compatible pdb
-antechamber -i RTZ.pdb -fi pdb -o RTZ.mol2 -fo mol2 -c bcc -s 2
+antechamber -i RTZ_file.pdb -fi pdb  -o RTZ.pdb -fo pdb -rn RTZ
 #using the bcc method to calculate the atomic point charge and generate mol2 for drug (RTZ)
-parmchk2 -i RTZ.mol2 -f mol2 -o RTZ.frcmod
+antechamber -i RTZ.pdb -fi pdb -o RTZ.mol2 -fo mol2 -c bcc -s 2
 #check if there are any missing parameters and generate frcmod file for drug (RTZ)
-pdb4amber -i P10635.B99990010.pdb -o Clean_P10635.B99990010.pdb --dry
+parmchk2 -i RTZ.mol2 -f mol2 -o RTZ.frcmod
 #final cleaning of CYP2D6 with Heme group prior running leap
-tleap -f tleap_ligands.in
+pdb4amber -i P10635.B99990010.pdb -o Clean_P10635.B99990010.pdb --dry
 #running leap
+tleap -f tleap_ligands.in
 
+#in vacuo minimisation
 echo "
 Test 
 &cntrl
@@ -44,42 +45,42 @@ reduce -Trim temporary.pdb >minimized_CYP2D6.pdb
 #in vacuo minimisation
 
 echo "
-source /opt/exp_soft/bioinf/amber20/dat/leap/cmd/oldff/leaprc.ff14SB
 #Source leaprc file for ff14SB protein force field
-source /opt/exp_soft/bioinf/amber20/dat/leap/cmd/leaprc.gaff
+source /opt/exp_soft/bioinf/amber20/dat/leap/cmd/oldff/leaprc.ff14SB
 #Source leaprc file for gaff
-source /opt/exp_soft/bioinf/amber20/dat/leap/cmd/leaprc.water.tip3p
+source /opt/exp_soft/bioinf/amber20/dat/leap/cmd/leaprc.gaff
 #source leaprc file for TIP3 water model
-HEM =loadmol2 HEM.mol2
+source /opt/exp_soft/bioinf/amber20/dat/leap/cmd/leaprc.water.tip3p
 #Load the mol2 file for HEM
-RTZ =loadmol2 RTZ.mol2
+HEM =loadmol2 HEM.mol2
 #Load the mol2 file for RTZ
-CYP =loadmol2 CYP.mol2
+RTZ =loadmol2 RTZ.mol2
 #Load the mol2 file for CYP
-loadamberparams frcmod.ionsjc_tip3p
+CYP =loadmol2 CYP.mol2
 #Load the ionsjc_tip3p frcmod file for water
-loadamberparams IC6.frcmod
+loadamberparams frcmod.ionsjc_tip3p
 #Load the IC6 frcmod file for the heme group
-loadamberparams RTZ.frcmod
+loadamberparams IC6.frcmod
 #Load the RTZ frcmod file for the drug molecules
+loadamberparams RTZ.frcmod
+#load minimized CYP2D6 pdb
 mol=loadpdb minimized_CYP2D6.pdb
-#Load PDB file for CYP2D6 bound to heme group and drug
 
+#generate bond between proximal cysteine and Fe
 bond mol.412.SG mol.469.FE
 bond mol.412.N mol.411.C
 bond mol.412.C mol.413.N
-#generate bond between proximal cysteine and Fe
 
+#add counterions to neutralise system and solvate box
 addions mol Cl- 0
 solvateoct mol TIP3PBOX 15.0
-#add counterions to neutralise system and solvate box
 
-saveamberparm mol CYP2D6_min_topology.prmtop CYP2D6_min_topology.rst7
 #Save AMBER topology and coordinate files
-savepdb mol CYP2D6_min_topology.pdb
+saveamberparm mol CYP2D6_min_topology.prmtop CYP2D6_min_topology.rst7
 #Save pdb output file
-quit
+savepdb mol CYP2D6_min_topology.pdb
 #Quit tleap program
+quit
 " >CYP2D6_min_topology.in
  
 tleap -f CYP2D6_min_topology.in
